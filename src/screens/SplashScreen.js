4459108013c38aab2874ccb786d13e55ea69d352
marcopeakdev <peakgenius226@gmail.com>
2024-11-21 05:38:32 -0700

    Added Splash screen and edited Splashi Dradient Icon

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AuthCenter from '../components/Auth/AuthCenter';
+import SplashGradientIcon from '../components/SplashGradientIcon';
+
+export default SplashScreen = () => {
+  return (
+    <AuthCenter justifyContent="center">
+      <SplashGradientIcon size="2xl" />
+    </AuthCenter>
+  );
+};
