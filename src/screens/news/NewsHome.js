05d7f903caa9622ca2b2241fa1e010ebfbc40a01
marcopeakdev <peakgenius226@gmail.com>
2024-11-21 05:51:21 -0700

    Added news home

---
+++ src/screens/news/NewsHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from 'native-base';
+
+const NewsHome = ({ navigation, route }, props) => {
+  return (
+    <Box>
+      <Text>News</Text>
+    </Box>
+  );
+};
+
+export default NewsHome;
